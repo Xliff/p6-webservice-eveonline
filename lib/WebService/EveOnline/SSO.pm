0a0e46d4355490a8c496f5a6a33fe56ba8463de9
clifton.wood@gmail.com <clifton.wood@gmail.com>
2016-11-24 12:53:44 -0500

    - Filling in the missing pieces from the main getToken routine.

--- lib/WebService/EveOnline/SSO.pm
+++ lib/WebService/EveOnline/SSO.pm
@@ -11,8 +11,10 @@ class WebService::EveOnline::SSO {
 
 	use WebService::EveOnline::Utils;
 
-	has $.postclient;
-	has $.client;
+	has $!postclient;
+	has $!client;
+	has $!xmlDoc;
+	has %!fieldsInForm;
 	has %.privateData;
 	has $.tokenData;
 
@@ -21,7 +23,7 @@ class WebService::EveOnline::SSO {
 			:max-redirects(5), :useragent<WebService::Eve v0.0.1 (rakudo)>
 		);
 
-		$postclient = HTTP::UserAgent.new(
+		$!postclient = HTTP::UserAgent.new(
 			:max-redirects(0), 
 			:useragent<WebService::Eve v0.0.1 (rakudo)>
 		);
@@ -41,6 +43,89 @@ class WebService::EveOnline::SSO {
 	method !getState {
 		# Override to implement custom state value generation.
 		'variable' ~ (^999).pick;
+	}
+
+	method !handleLogin {
+		my @forms = $xmldoc.elements(:TAG<form>, :RECURSE(100));
+		my $form_data = {
+		 	ClientIdentifier 	=> %.privateData<client_id>,
+		 	UserName 			=> %.privateData<username>,
+		 	Password			=> %.privateData<password>,
+		 	RememberMe			=> 'false'
+		};
+
+		my $formUrl = @forms[0]<action>;
+		unless $formUrl ~~ /^ 'http' s? '://' / {
+			# cw: Will more than likely work for EVE, but NOT a real solution.
+			$formUrl = "{ $prefix }{ $formUrl }";
+		}
+
+		try {
+			# cw: This doesn't seem to follow the same pattern as the last 
+			#     request. There may not be an exception, here.
+			CATCH {
+				when X::HTTP::Response { .resume }
+			}
+
+			$response = $!postclient.post($formUrl, $form_data);
+			# cw: We've rolled our own until a fix can be made for HTTP::UserAgent
+			#     Probably has to do with early expiration or parsing in 
+			#     HTTP::Cookies.
+			my @cookies = getCookies($response);
+			
+			# cw: This will be a redirect, but it needs to be a GET, not
+			#     a POST.
+			for @cookies -> $c {
+				# cw: Wrap in a DEBUG.
+				#say "Cookie { $c.name } expires '{ $c.expires }'";
+
+				$!client.cookies.push-cookie($c);
+				$!postclient.cookies.push-cookie($c);
+			}
+			
+			# cw: Not optimal, but this should generally work for the servers
+			#     we connect to.
+			$url = $response.header.field('Location');
+			unless $url ~~ /^^ 'https://' / {
+				# cw: MUST be HTTPS at this point.
+				$url = "{ $prefix }{ $url }"
+			}
+
+			# cw: -YYY-
+			#     At this point, if URL has "/Account/LogOn" in it, then we've 
+			#     failed the authorization phase and should throw an exception.
+		}
+
+		try {
+			CATCH {
+				when X::HTTP::NoResponse {
+					$response = .response;
+				}
+			}
+
+			$response = $client.get($url);
+
+			die "HTTP request to '$url' failed!" unless $response.is-success;
+
+			$content = $response.content;
+		}
+
+		my @cookies = getCookies($response);
+		for @cookies -> $c {
+			$!client.cookies.push-cookie($c);
+			$!postclient.cookies.push-cookie($c);
+		}
+		$!xmldoc = HTML::Parser::XML.new.parse($content);
+		@fields = $xmldoc.elements(:TAG<input>, :RECURSE<100>); 
+		@sel = $xmldoc.elements(:TAG<select>, :RECURSE<100>);
+
+		%!fieldsInForm = ();
+		for |(@fields, @sel).flat -> $f {
+			next unless $f<name>.defined;
+			%!fieldsInForm{$f<name>} = 1;
+		}
+
+		$response;
 	}
 	
 	method getToken is export {
@@ -75,24 +160,25 @@ class WebService::EveOnline::SSO {
 		$content = $response.content;
 
 		my $tokenCode;
-		my %found;
-		my @forms = $xmldoc.elements(:TAG<form>, :RECURSE(100));		
-		my $xmldoc = HTML::Parser::XML.new.parse($content);
-		my @fields = $xmldoc.elements(:TAG<input>, :RECURSE<100>); 
-		my @sel = $xmldoc.elements(:TAG<select>, :RECURSE<100>);
+				
+		my $!xmldoc = HTML::Parser::XML.new.parse($content);
+		my @fields = $!xmldoc.elements(:TAG<input>, :RECURSE<100>); 
+		my @sel = $!xmldoc.elements(:TAG<select>, :RECURSE<100>);
 
 		for |(@fields, @sel).flat -> $f {
 			next unless $f<name>.defined;
-			%found{$f<name>} = 1;
+			%!fieldsInForm{$f<name>} = 1;
 		}
 
-		$response = self!handleLogin if 
-			%found<UserName> 			&& 
-			%found<Password> 			&&
-			%found<RememberMe>			&&
-			%found<ClientIdentifier>;
+		$response = self!handleLogin 
+			if 
+				%!fieldsInForm<UserName> 			&& 
+				%!fieldsInForm<Password> 			&&
+				%!fieldsInForm<RememberMe>			&&
+				%!fieldsInForm<ClientIdentifier>;
 		
-		$response = self!charSelect if %found<CharacterId>;
+		$response = self!handleCharSelect 
+			if %!fieldsInForm<CharacterId>;
 
 		# At this point, the redirect should contain a code parameter. 
 		# Search for it.
