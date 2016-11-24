7644276ab4c4b569213af281c1e0d81a2a2f081a
clifton.wood@gmail.com <clifton.wood@gmail.com>
2016-11-24 11:40:48 -0500

    - Putting in the broad strokes for SSO, based on the oauth testing script.

--- lib/WebService/EveOnline/SSO.pm
+++ lib/WebService/EveOnline/SSO.pm
@@ -6,6 +6,7 @@ class WebService::EveOnline::SSO {
 	use HTML::Parser::XML;
 	use HTTP::UserAgent;
 	use HTTP::Cookies;
+	use JSON::Fast;
 	use XML;
 
 	use WebService::EveOnline::Utils;
@@ -13,6 +14,7 @@ class WebService::EveOnline::SSO {
 	has $.postclient;
 	has $.client;
 	has %.privateData;
+	has $.tokenData;
 
 	submethod BUILD {
 		$!client = HTTP::UserAgent.new(
@@ -66,6 +68,62 @@ class WebService::EveOnline::SSO {
 		my $url = "{ $prefix }oauth/authorize?{ $p }";
 		my $response;
 		my $content;
+
+		$response = $client.get($url);
+		# cw: Should throw an exception, instead.
+		die "HTTP request to '$url' failed!" unless $response.is-success;	
+		$content = $response.content;
+
+		my $tokenCode;
+		my %found;
+		my @forms = $xmldoc.elements(:TAG<form>, :RECURSE(100));		
+		my $xmldoc = HTML::Parser::XML.new.parse($content);
+		my @fields = $xmldoc.elements(:TAG<input>, :RECURSE<100>); 
+		my @sel = $xmldoc.elements(:TAG<select>, :RECURSE<100>);
+
+		for |(@fields, @sel).flat -> $f {
+			next unless $f<name>.defined;
+			%found{$f<name>} = 1;
+		}
+
+		$response = self!handleLogin if 
+			%found<UserName> 			&& 
+			%found<Password> 			&&
+			%found<RememberMe>			&&
+			%found<ClientIdentifier>;
+		
+		$response = self!charSelect if %found<CharacterId>;
+
+		# At this point, the redirect should contain a code parameter. 
+		# Search for it.
+		my $loc = $response.header.field('Location') // '';
+		if $loc ~~ / 'code=' (<-[ & ]>+) / {
+			$tokenCode = $/[0];
+		}
+
+		# cw: Should be an exception
+		die "Could not find tokenCode in response data"
+			unless $tokenCode.chars;
+
+		my $basic = encode-base64(
+			"{ %privateData<client_id> }:{ %privateData<secret_id> }", :str
+		);
+		my $form_data = {
+			grant_type	=> 'authorization_code',
+			code 		=> $tokenCode, 
+		};
+
+		$response = $postclient.post(
+			"{ $prefix }/oauth/token", 
+			$form_data, 
+			:Authorization("Basic $basic"),
+		);
+
+		# cw: Should be an exception
+		die "Token not retrieved due to unexpected error."
+			unless $response.is-success;
+
+		$!tokenData = from-json($response.content);
 	}
 
 }
